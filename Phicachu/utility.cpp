47f0f3f17a6ec3bef91729aff5406a520ef1cc36
baodata <164466777+baodata@users.noreply.github.com>
2024-04-17 11:18:45 +0700

    Add files via upload

--- Phicachu/utility.cpp
+++ Phicachu/utility.cpp
@@ -112,21 +112,9 @@ void countDown(int *s)
     }
 }
 
-wstring s2ws(const std::string& s) // https://stackoverflow.com/questions/62762067/using-string-variables-for-the-function-playsound
-{
-    int len;
-    int slength = (int)s.length() + 1;
-    len = MultiByteToWideChar(CP_ACP, 0, s.c_str(), slength, 0, 0);
-    wchar_t* buf = new wchar_t[len];
-    MultiByteToWideChar(CP_ACP, 0, s.c_str(), slength, buf, len);
-    std::wstring r(buf);
-    delete[] buf;
-    return r;
-}
-
 void makeSound(string sound)    //https://stackoverflow.com/questions/9961949/playsound-in-c-console-application 
 {
-    PlaySound(s2ws(sound).c_str(), NULL, SND_FILENAME| SND_ASYNC);
+    PlaySoundA(sound.c_str(), NULL, SND_FILENAME| SND_ASYNC);
 }
 
 void setMaxNumber(int & x, int max)
